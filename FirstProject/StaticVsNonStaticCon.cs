4947f3f7309116785b26813b1bc37f98e693c41f
vaibhavlvl1 <acevaibhav14@gmail.com>
2026-03-10 22:51:50 +0530

    static and nonstatic added completed

---
+++ FirstProject/StaticVsNonStaticCon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class StaticVsNonStaticCon
+    {
+        int X;
+        static int Y;
+        static StaticVsNonStaticCon()
+        {
+            Console.WriteLine("Static Construtor Called Only 1 time");
+        }
+
+        public StaticVsNonStaticCon()
+        {
+            Console.WriteLine("Non Static Construcotr gettting called with every created Instance");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("NS constructor is now called ");
+        }
+
+        static void Main()
+        {
+            // We can directly use value of a static variable in static method in class. because the varaible is initilized single time 
+            // by static constructor. But we cant do that to NS variable because it gets initialized with all the instances
+            // and concufion ensues which value are we refering to 
+            Console.WriteLine(Y);
+
+            StaticVsNonStaticCon NS = new StaticVsNonStaticCon();
+
+            //NS.Display();
+        }
+
+      
+
+        
+
+        //NS.Display();
+
+    }
+}
--- FirstProject/obj/Debug/net8.0/FirstProject.AssemblyInfo.cs
+++ FirstProject/obj/Debug/net8.0/FirstProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f01e82d52c4c7ff63700503853cd8aecf262670d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
