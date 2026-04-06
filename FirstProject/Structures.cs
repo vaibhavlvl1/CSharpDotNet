1ed7fe0316a3ce0418900844c7daa9e5dd3b6313
vaibhavlvl1 <acevaibhav14@gmail.com>
2026-04-06 11:15:58 +0530

    structures added

--- FirstProject/Structures.cs
+++ FirstProject/Structures.cs
@@ -6,6 +6,13 @@ namespace FirstProject
     struct SimpleStructure
     {
         int i;  // cant assign the value direclty beacise it wont be initialized cause instance not created by new keyword
+
+         public SimpleStructure(int i)
+        {
+            this.i = i;
+            Console.WriteLine("This is a constructor inside structurewit value of i " + i);
+        }
+
         public void Display()
         {
             Console.WriteLine("Method in a struct "+ i);
@@ -17,8 +24,13 @@ namespace FirstProject
             // IT is optional 
 
             SimpleStructure SS; // No new keyword
-            SS.i = 25; // have to initailized variables like this because new keyword not used.
+            SS.i = 25; // have to initailized variables like this because new keyword not used. Else will give error in build
             SS.Display();
+
+
+            // This new instance will call the constructor and set value of i to be 5
+            SimpleStructure SS1 = new SimpleStructure(5);
+            SS1.Display();
         }
     }
 }
