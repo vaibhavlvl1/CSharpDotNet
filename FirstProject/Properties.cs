b59585512409f5bd29680fd2cc727c58806a7eb7
vaibhavlvl1 <acevaibhav14@gmail.com>
2026-04-16 07:43:41 +0530

    added properties 1,2

---
+++ FirstProject/Properties.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+
+    class PropertiesDemo
+    {   
+        // This variable has to accessible to other classes
+        double SecretKey = 245.17;  // If we set it public it will be accesible but can be changed as well
+
+        // Underscore is needed if we are using a property to get and set
+
+        double _SecretKey2 = 245.17;
+
+        // defining methods to get and edit value of SecretKey
+
+        public void SetSecretKey(double value)
+        {
+            SecretKey = value;
+        }
+
+        public double GetSecretKey()
+        {
+            return SecretKey;
+        }
+
+        // Lets make property which will combine above separate functions
+
+        //public property To get field _SecretKey1
+
+        public double SecretKey1Property
+        {
+            get { return _SecretKey2; }
+            set {
+
+                if (_SecretKey2 != value) { _SecretKey2 = value; }
+                // This value is implicitly declared in run time.
+            
+            }
+
+
+            // IF there are not coditions We can also write as follows. minimally
+
+            // get;set;
+
+        }
+
+    }
+
+     class PropertiesDemo1 
+    {
+        static void Main()
+        {
+            // Creating a instance of properties demo and access the secret key;
+            PropertiesDemo pd = new PropertiesDemo();
+
+            // pd.SecretKey = 21;    This values is not accessible since secretkey in private. So we use the defined methods.
+
+            Console.WriteLine("Secret key defined in one class viewed inside other class " + pd.GetSecretKey());
+
+            pd.SetSecretKey(10);
+
+            Console.WriteLine("Secret key set in other class " + pd.GetSecretKey());
+
+            // Instead of writint separate methods We can make a Property with both get and set fields instead of making
+            // two separate properties
+
+            Console.WriteLine("Getting secretKey 2  " + pd.SecretKey1Property);
+
+            // Setting secret key 2
+            pd.SecretKey1Property = 10;
+            Console.WriteLine("Gettig Secret key again after setting  " + pd.SecretKey1Property);
+
+        }
+    }
+}
---
+++ FirstProject/Properties2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+
+    class Customer
+    {
+        int _CustId;
+        string _CName;
+        bool _IsActive;
+        double _Balance;
+        // The underscores are used to let the reader know that these variables have properties to set them
+
+        // One of the advantages of properties is we can conditionllay assign them values
+
+        public int CustId{
+            get { return _CustId; }
+        }
+
+        public double Balance
+        {
+            get { return _Balance; }
+
+            set {
+
+                if (_IsActive)
+                {
+                    _Balance = value;
+                    Console.WriteLine("Balance Updated");
+                }
+                else
+                {
+                    Console.WriteLine("User Not Active Unable to update balance");
+                }
+            
+            }
+        }
+
+        public bool IsActive
+        {
+            get { return _IsActive; }
+            set { _IsActive = value; }
+        }
+
+        public string CName
+        {
+            get { return _CName; }
+            set {
+
+                if (_IsActive) { _CName = value; }
+
+
+                else {
+                    Console.WriteLine("Name cannot be Changed because user inactive");
+            }
+            } }
+
+
+
+        public  Customer(int CustId, string CName, bool IsActive,double Balance)
+        { 
+            this._CustId = CustId;
+            this._Balance = Balance; 
+            this._CName = CName;
+            this._IsActive = IsActive;
+            
+        }
+            
+    }
+    
+
+    internal class Properties2
+    {
+        static void Main()
+        {
+            Customer C = new Customer(1, "Yohan", true, 10000);
+
+            Console.WriteLine("Customer Name " + C.CName);
+
+            Console.WriteLine("Changing name to Yohan Blake using property");
+
+            C.CName = "Yohan Blake";
+
+            Console.WriteLine("Customer new name is " + C.CName);
+
+            Console.WriteLine("Chagning User name to Yohan Bake to not active so change name not possible");
+
+            C.IsActive = false;
+
+            C.CName = "Bolt";
+
+
+        }
+       
+        
+
+    }
+}
