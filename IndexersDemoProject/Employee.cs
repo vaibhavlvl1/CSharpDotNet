1479bc1ac8ee31df6e38ebf7023d836115af9e9e
vaibhavlvl1 <acevaibhav14@gmail.com>
2026-04-20 22:43:41 +0530

    Added Indexers project

---
+++ IndexersDemoProject/Employee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexersDemoProject
+{
+    internal class Employee
+    {
+        int Id;
+        string Name, Address, City, Designation;
+        double Salary;
+
+        public Employee(int Id,string Name,string Address,string City,string Designation,double Salary)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Address = Address;
+            this.City = City;
+            this.Designation = Designation;
+            this.Salary = Salary;
+        }
+
+        // Now defining the indexer for Employee class to set and get
+
+        // We can also use string name instead of int index; just make sure to use ToLower ot upper
+        // as string name will be case sensitive
+
+        public object this[int index]
+        {   
+
+            //to get value
+            get
+            {
+                if (index == 0)
+                {
+                    return Id;
+                }
+                else if (index == 1)
+                {
+                    return Name;
+                }
+                else if (index == 2)
+                {
+                    return Address;
+                }
+                else if (index == 3)
+                {
+                    return City;
+                }
+                else if (index == 4)
+                {
+                    return Designation;
+                }
+
+                else if (index == 5)
+                {
+                    return Salary;
+                }
+
+                return null;
+            }
+
+
+            //to set value
+            set
+            {
+                if (index == 0)
+                {
+                    Id = (int)value;
+                }
+                else if (index == 1)
+                {
+                    Name = (string)value;
+                }
+                else if (index == 2)
+                {
+                    Address = (string)value;
+                }
+
+                else if (index == 3)
+                {
+                    City = (string)value;
+                }
+                else if (index == 4)
+                {
+                    Designation = (string)value;
+                }
+                else if(index == 5)
+                {
+                    Salary = (int)value;
+                }
+
+            }
+        }
+    }
+}
---
+++ IndexersDemoProject/Indexer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexersDemoProject
+{
+    internal class Indexer
+    {
+        static void Main()
+        {
+            Employee E = new Employee(1,"Jason","USA","Illinois","Head of Strss",900000);
+
+            // Inddexers has been defined in the class Employtee check it out
+
+            Console.WriteLine(E[0]);
+            Console.WriteLine(E[1]);
+            Console.WriteLine(E[2]);
+            Console.WriteLine(E[3]);
+            Console.WriteLine(E[4]);
+
+            //setting some random values to the varibles using the indexers
+
+            E[0] = 555;
+            E[1] = "Statham";
+            E[2] = "Iceland";
+
+            Console.WriteLine(E[0]);
+            Console.WriteLine(E[1]);
+            Console.WriteLine(E[2]);
+            Console.WriteLine(E[3]);
+            Console.WriteLine(E[4]);
+
+        }
+    }
+}
