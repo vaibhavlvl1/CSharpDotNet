cd249b38c28ef47798dc5e9eee93dd12bc8e2e01
vaibhavlvl1 <acevaibhav14@gmail.com>
2026-04-29 13:25:03 +0530

    added string builders

--- ExtensionMethodsProject/obj/Debug/net8.0/ExtensionMethodsProject.AssemblyInfo.cs
+++ ExtensionMethodsProject/obj/Debug/net8.0/ExtensionMethodsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExtensionMethodsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc934449a0e872171d799b73b1d3fc87a17f9f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9310d9815bea95bc0db3b228e202ddc5f2d5298d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExtensionMethodsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExtensionMethodsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FirstProject/StringVStringBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class StringVStringBuilder
+    {
+        static void Main()
+        {   
+            // lets use a string and modify it 100 times and log its time
+            String str = "Hello";
+
+            // the below string addition will make 100 copies of a string. lets log the time taken for this suign stopwatch class
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            for (int i=1;i<= 50000; i++)
+            {
+                str = str + "1";
+            }
+            sw.Stop();
+
+            // Now lets use above example using a string builder
+
+            StringBuilder sbString = new StringBuilder();
+            sbString.Append("Hello");
+
+            Stopwatch sw1 = new Stopwatch();
+
+            sw1.Start();
+            for (int i = 1; i <= 50000; i++)
+            {
+                sbString.Append("1");
+            }
+            sw1.Stop();
+
+            Console.WriteLine("Operation on String took " + sw.ElapsedMilliseconds);
+            Console.WriteLine("Operation on String builder took " + sw1.ElapsedMilliseconds);
+            
+        }
+    }
+}
